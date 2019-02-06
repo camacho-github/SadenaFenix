c7b1b219dd3f1b7d1a358c5d604708d26e37e91c
Jalboros@ <jalboros@gmail.com>
2019-02-06 05:03:22 -0600

    Actions de consulta de reportes por municipios

--- SadenaFenix/Controllers/Nacimientos/ReportesController.cs
+++ SadenaFenix/Controllers/Nacimientos/ReportesController.cs
@@ -1,5 +1,10 @@
-﻿using SadenaFenix.Models.Usuarios;
+﻿using SadenaFenix.Models.Catalogos.Geografia;
+using SadenaFenix.Models.Usuarios;
+using SadenaFenix.Services;
+using SadenaFenix.Transport.Nacimientos.Reportes;
+using System.Collections.ObjectModel;
 using System.Web.Mvc;
+using System.Web.Services;
 
 namespace Sadena.Controllers.Nacimientos
 {
@@ -90,6 +95,137 @@ namespace Sadena.Controllers.Nacimientos
             {
                 return View();
             }
+        }
+
+
+        [WebMethod]
+        public ActionResult ConsultarReporteSexoSubregistro()
+        {
+            Servicio servicio = new Servicio();
+            ReporteSexoSubregistroPeticion peticion = new ReporteSexoSubregistroPeticion();
+            Collection<string> ColAnos = new Collection<string>();
+            ColAnos.Add("2017");
+            ColAnos.Add("2018");
+
+            Collection<string> ColMeses = new Collection<string>();
+            ColMeses.Add("1");
+            ColMeses.Add("2");
+            ColMeses.Add("3");
+
+            Collection<Municipio> ColMunicipio = new Collection<Municipio>();
+
+            peticion.ColAnos = ColAnos;
+            peticion.ColMeses = ColMeses;
+            peticion.ColMunicipios = ColMunicipio;
+
+            ReporteSubregistroRespuesta respuesta = new ReporteSubregistroRespuesta();
+            respuesta = servicio.ConsultarReporteSexoSubregistro(peticion);
+
+            return Json(respuesta, JsonRequestBehavior.AllowGet);
+        }
+
+        [WebMethod]
+        public ActionResult ConsultarReporteEdoCivilSubregistro()
+        {
+            Servicio servicio = new Servicio();
+            ReporteEdoCivilSubregistroPeticion peticion = new ReporteEdoCivilSubregistroPeticion();
+            Collection<string> ColAnos = new Collection<string>();
+            ColAnos.Add("2017");
+            ColAnos.Add("2018");
+
+            Collection<string> ColMeses = new Collection<string>();
+            ColMeses.Add("1");
+            ColMeses.Add("2");
+            ColMeses.Add("3");
+
+            Collection<Municipio> ColMunicipio = new Collection<Municipio>();
+
+            peticion.ColAnos = ColAnos;
+            peticion.ColMeses = ColMeses;
+            peticion.ColMunicipios = ColMunicipio;
+
+            ReporteSubregistroRespuesta respuesta = new ReporteSubregistroRespuesta();
+            respuesta = servicio.ConsultarReporteEdoCivilSubregistro(peticion);
+
+            return Json(respuesta, JsonRequestBehavior.AllowGet);
+        }
+
+        [WebMethod]
+        public ActionResult ConsultarReporteEdadSubregistro()
+        {
+            Servicio servicio = new Servicio();
+            ReporteEdadSubregistroPeticion peticion = new ReporteEdadSubregistroPeticion();
+            Collection<string> ColAnos = new Collection<string>();
+            ColAnos.Add("2017");
+            ColAnos.Add("2018");
+
+            Collection<string> ColMeses = new Collection<string>();
+            ColMeses.Add("1");
+            ColMeses.Add("2");
+            ColMeses.Add("3");
+
+            Collection<Municipio> ColMunicipio = new Collection<Municipio>();
+
+            peticion.ColAnos = ColAnos;
+            peticion.ColMeses = ColMeses;
+            peticion.ColMunicipios = ColMunicipio;
+
+            ReporteSubregistroRespuesta respuesta = new ReporteSubregistroRespuesta();
+            respuesta = servicio.ConsultarReporteEdadSubregistro(peticion);
+
+            return Json(respuesta, JsonRequestBehavior.AllowGet);
+        }
+
+        [WebMethod]
+        public ActionResult ConsultarReporteEscolaridadSubregistro()
+        {
+            Servicio servicio = new Servicio();
+            ReporteEscolaridadSubregistroPeticion peticion = new ReporteEscolaridadSubregistroPeticion();
+            Collection<string> ColAnos = new Collection<string>();
+            ColAnos.Add("2017");
+            ColAnos.Add("2018");
+
+            Collection<string> ColMeses = new Collection<string>();
+            ColMeses.Add("1");
+            ColMeses.Add("2");
+            ColMeses.Add("3");
+
+            Collection<Municipio> ColMunicipio = new Collection<Municipio>();
+
+            peticion.ColAnos = ColAnos;
+            peticion.ColMeses = ColMeses;
+            peticion.ColMunicipios = ColMunicipio;
+
+            ReporteSubregistroRespuesta respuesta = new ReporteSubregistroRespuesta();
+            respuesta = servicio.ConsultarReporteEscolaridadSubregistro(peticion);
+
+            return Json(respuesta, JsonRequestBehavior.AllowGet);
+        }
+
+        [WebMethod]
+        public ActionResult ConsultarReporteNumNacSubregistro()
+        {
+            Servicio servicio = new Servicio();
+            ReporteNumNacSubregistroPeticion peticion = new ReporteNumNacSubregistroPeticion();
+            Collection<string> ColAnos = new Collection<string>();
+            ColAnos.Add("2017");
+            ColAnos.Add("2018");
+
+            Collection<string> ColMeses = new Collection<string>();
+            ColMeses.Add("1");
+            ColMeses.Add("2");
+            ColMeses.Add("3");
+
+            Collection<Municipio> ColMunicipio = new Collection<Municipio>();
+
+            peticion.ColAnos = ColAnos;
+            peticion.ColMeses = ColMeses;
+            peticion.ColMunicipios = ColMunicipio;
+
+            ReporteSubregistroRespuesta respuesta = new ReporteSubregistroRespuesta();
+            respuesta = servicio.ConsultarReporteNumNacSubregistro(peticion);
+
+            return Json(respuesta, JsonRequestBehavior.AllowGet);
         }
     }
 }
